ccb1d5942ac0a2d8002c17b121b70d8c80804210
ag999 <ag999@cornell.edu>
2018-04-17 17:56:12 -0400

    first attemp at getting outdated events

--- backend_main/views.py
+++ backend_main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from reversion.models import Version
 
 from .models import Org
 
@@ -17,3 +18,6 @@ def locationDetail(request,location_id):
     location_set = Location.objects.get(pk=location_id)
     serializer = EventSerialize(event_set,many=False)
     return JsonResponse(serializer.data,status=status.HTTP_200_OK,safe=False)
+
+def changesInEvents(timestamp, start_time, end_time):
+    version_set = Version.objects.get_for_model(models.Event, model_db=None)
