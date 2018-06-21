849843f7b5d8348a2c3fb0a7fc8afac011346721
Arnav Ghosh <garnav@gmail.com>
2018-06-22 00:09:10 +0530

    Re-upload from heroku - all new serializers

--- backend_main/views.py
+++ backend_main/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.template import loader
+# serializers.py
+# Jessica Zhao, Adit Gupta, Arnav Ghosh
+# 21st June 2018
 
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.template import loader
+from django.utils import timezone
+
+from rest_framework.renderers import JSONRenderer
+from rest_framework import status
 
 from .models import Org, Event
-
-#from rest_framework import status
-
-# Create your views here.
+from .serializers import EventSerializer, LocationSerializer, OrgSerializer,
+                        UpdatedEventsSerializer, UpdatedOrgSerializer
 
 def eventDetail(request,event_id):
     event_set = Event.objects.get(pk=event_id)
@@ -16,30 +21,44 @@ def eventDetail(request,event_id):
 
 def locationDetail(request,location_id):
     location_set = Location.objects.get(pk=location_id)
-    serializer = EventSerialize(event_set,many=False)
+    serializer = EventSerialize(location_set,many=False)
     return JsonResponse(serializer.data,status=status.HTTP_200_OK,safe=False)
 
+def orgDetail(request,org_id):
+    org_set = Location.objects.get(pk=org_id)
+    serializer = EventSerializer(org_set,many=False)
+    return JsonResponse(serializer.data,status=status.HTTP_200_OK,safe=False)
 
+def changesInOrgs(timestamp):
+    outdated_orgs, all_deleted = outdatedOrgs(timestamp)
+    json_orgs = JSONRenderer().render(OrgSerializer(outdated_orgs, many = True).data)
+    serializer = UpdatedOrgSerializer(updated = json_orgs, deleted = all_deleted, timestamp = timezone.now())
+    return JsonResponse(serializer.data,status=status.HTTP_200_OK,safe=False)
 
-def changesInOrgs(timestamp, start_time, end_time):
-    outdated_orgs = outdatedOrgs(timestamp)
-    
-def outdatedOrgs(timestamp, start_time,end_time):
-    org_updates = Org.history.filter(timestamp__gte = timestamp);
-    org_updates = org_updates.distinct('id');
-    
-    org_list = org_updates.values_list('id');
-    changed_orgs = Org.objects.filter(pk__in=org_list, start_date__gte=start_time,end_date__gte=end_time);
+def outdatedOrgs(timestamp):
+    org_updates = Org.history.filter(timestamp__gte = timestamp)
+    org_updates = org_updates.distinct('id')
+
+    org_list = org_updates.values_list('id')
+    #TODO: What if not in list
+    changed_orgs = Org.objects.filter(pk__in=org_list)
+    present_pks = Org.objects.filter(pk__in = org_list).value_list('pk', flat = True)
+    all_deleted_pks = list(set(org_list).difference(set(present_pks)))
+    return changed_orgs, all_deleted_pks
                                                
-    
-
 def changesInEvents(timestamp, start_time, end_time):
-    outdated_events = outdatedEvents(timestamp)
+    outdated_events, all_deleted = outdatedEvents(timestamp, start_time, end_time)
+    json_events = JSONRenderer().render(EventSerializer(outdated_events, many = True).data)
+    serializer = UpdatedEventsSerializer(updated = json_events, deleted = all_deleted, timestamp = timezone.now())
+    return JsonResponse(serializer.data,status=status.HTTP_200_OK,safe=False)
 
 def outdatedEvents(timestamp, start_time, end_time):
     history_set = Event.history.filter(history_date__gte = timestamp)
     unique_set  = history_set.distinct('id')
-    pks = unique_set.values_list('id', flat=True).order_by('id')
-    changed_events = Event.objects.filter(pk__in = pks, start_date__gte = start_time, end_date__lte =  end_time)
-     
 
+    pks = unique_set.values_list('id', flat=True).order_by('id')
+    #TODO: What if not in list
+    changed_events = Event.objects.filter(pk__in = pks, start_date__gte = start_time, end_date__lte =  end_time)
+    present_pks = Event.objects.filter(pk__in = pks).value_list('pk', flat = True)
+    all_deleted_pks = list(set(pks).difference(set(present_pks)))
+    return changed_events, all_deleted_pks
