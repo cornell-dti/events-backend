60474db73bdec9dc7081c558dd3576ba5422c077
Arnav Ghosh <ag983@cornell.edu>
2018-09-01 15:00:07 -0400

    Test image downloads

--- backend_main/admin.py
+++ backend_main/admin.py
@@ -3,9 +3,10 @@
 # 16th July 2018
 
 from django.contrib import admin
-from .models import Org, Event, Location, Media
+from .models import Org, Event, Location, Media, Tag
 
 admin.site.register(Org)
 admin.site.register(Event)
 admin.site.register(Location)
 admin.site.register(Media)
+admin.site.register(Tag)
