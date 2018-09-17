59e9d245ccf662b15f752ae9c188decf798bef84
Arnav Ghosh <ag983@cornell.edu>
2018-09-16 20:28:35 -0400

    Added UserID for user-firebaseid linking, Inc. attendance via token

--- backend_main/admin.py
+++ backend_main/admin.py
@@ -3,10 +3,24 @@
 # 16th July 2018
 
 from django.contrib import admin
-from .models import Org, Event, Location, Media, Tag
+from .models import Attendance, Event, Location, Media, Org, Tag, UserID
+#from rest_framework.authtoken.admin import Tokenadmin
+#from rest_framework.authtoken.admin import Token
 
 admin.site.register(Org)
 admin.site.register(Event)
 admin.site.register(Location)
 admin.site.register(Media)
 admin.site.register(Tag)
+admin.site.register(Attendance)
+admin.site.register(UserID)
+#admin.site.register(Token, TokenAdmin)
+
+#admin.site.unregister(Token) #First unregister the old class
+#admin.site.register(Token, AuthTokenAdmin) #Then register the new class
+
+# @admin.register(Token)
+# class TokenAdmin(admin.ModelAdmin):
+#     list_display = ('key', 'user', 'created')
+#     fields = ('user',)
+#     ordering = ('-created',)
--- backend_main/urls.py
+++ backend_main/urls.py
@@ -13,17 +13,17 @@ urlpatterns = [
 	#path('post/tag', views.post_tag, name='post_tag'),
 	#path('post/org/<int:pk>/edit/', views.post_org_edit, name='post_edit'),
 	#path('post/event/<int:pk>/edit/', views.post_event_edit, name='post_edit'),
-	url(r'^event/(?P<event_id>[0-9]+)/$', views.eventDetail, name='Event Details'),
+	url(r'^event/(?P<event_id>[0-9]+)/$', views.EventDetail.as_view(), name='Event Details'),
 	url(r'^org/(?P<org_id>[0-9]+)/$', views.orgDetail, name='Organizer Details'),
 	url(r'^loc/(?P<location_id>[0-9]+)/$', views.locationDetail, name='Location Details'),
 	url(r'^tag/(?P<tag_id>[0-9]+)/$', views.singleTag, name='Single Tag Details'),
 	url(r'^tag/all/$', views.allTags, name='Single Tag Details'),
 	url(r'^media/(?P<img_id>[0-9]+)/$', views.imageDetail, name='Media Detail'),
-	url(r'^feed/events/timestamp=(?P<in_timestamp>{0})&start=(?P<start_time>{0})&end=(?P<end_time>{0})/$'.format(dateRegex), 
-		views.changesInEvents, 
+	url(r'^feed/events/timestamp=(?P<in_timestamp>{0})&start=(?P<start_time>{0})&end=(?P<end_time>{0})/$'.format(dateRegex),
+		views.changesInEvents,
 		name='Updated Event Feed'),
 	url(r'^feed/org/timestamp=(?P<in_timestamp>{0})/$'.format(dateRegex), views.changesInOrgs, name='Updated Organizer Feed'),
 	url(r'^generate_token/(?P<mobile_id>.*)/$', views.createToken, name='Create Mobile Token'),
-	url(r'^attendance/$', views.incrementAttendance, name="Update Attendance")
+	url(r'^attendance/$', views.IncrementAttendance.as_view(), name="Update Attendance")
 
 ]
