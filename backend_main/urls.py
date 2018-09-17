fbf0195488131f2143e3b897e1ebe0ca8985060d
Arnav Ghosh <ag983@cornell.edu>
2018-09-17 09:12:27 -0400

    Updated all views to APIView

--- backend_main/urls.py
+++ backend_main/urls.py
@@ -16,14 +16,14 @@ urlpatterns = [
 	url(r'^event/(?P<event_id>[0-9]+)/$', views.EventDetail.as_view(), name='Event Details'),
 	url(r'^org/(?P<org_id>[0-9]+)/$', views.OrgDetail.as_view(), name='Organizer Details'),
 	url(r'^loc/(?P<location_id>[0-9]+)/$', views.LocationDetail.as_view(), name='Location Details'),
-	url(r'^tag/(?P<tag_id>[0-9]+)/$', views.singleTag, name='Single Tag Details'),
-	url(r'^tag/all/$', views.allTags, name='Single Tag Details'),
-	url(r'^media/(?P<img_id>[0-9]+)/$', views.imageDetail, name='Media Detail'),
+	url(r'^tag/(?P<tag_id>[0-9]+)/$', views.SingleTagDetail.as_view(), name='Single Tag Details'),
+	url(r'^tag/all/$', views.AllTagDetail.as_view(), name='Single Tag Details'),
+	url(r'^media/(?P<img_id>[0-9]+)/$',  views.ImageDetail.as_view(), name='Media Detail'),
 	url(r'^feed/events/timestamp=(?P<in_timestamp>{0})&start=(?P<start_time>{0})&end=(?P<end_time>{0})/$'.format(dateRegex),
 		views.EventFeed.as_view(),
 		name='Updated Event Feed'),
 	url(r'^feed/org/timestamp=(?P<in_timestamp>{0})/$'.format(dateRegex), views.OrgFeed.as_view(), name='Updated Organizer Feed'),
-	url(r'^generate_token/(?P<mobile_id>.*)/$', views.createToken, name='Create Mobile Token'),
+	url(r'^generate_token/(?P<mobile_id>.*)/$', views.ObtainToken.as_view(), name='Create Mobile Token'),
 	url(r'^attendance/$', views.IncrementAttendance.as_view(), name="Update Attendance")
 
 ]
