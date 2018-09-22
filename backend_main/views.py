f77688ef7a433197041ab127d5313b611c62efbe
Arnav Ghosh <ag983@cornell.edu>
2018-09-22 14:11:50 -0400

    Changed styling

--- backend_main/views.py
+++ backend_main/views.py
@@ -348,7 +348,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return HttpResponse(status=status.HTTP_200_OK)
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
