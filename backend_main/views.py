6aa51dc194a9f74eb63fa8ddaea313d1a3787aca
Arnav Ghosh <ag983@cornell.edu>
2018-09-22 13:46:37 -0400

    User signup

--- backend_main/views.py
+++ backend_main/views.py
@@ -10,12 +10,13 @@ import dateutil.parser
 
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.utils import timezone
 from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
-
 
 from rest_framework.renderers import JSONRenderer
 
@@ -335,3 +336,19 @@ class LocationFormView(APIView):
         else:
             form = LocationForm()
         return render(request, 'post_edit.html', {'form': form})
+
+=============================================
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
