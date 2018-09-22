707538545b629ba9d21aa2ebf8892d700f32bec7
Jill Wu <gena168@gmail.com>
2018-09-22 17:15:58 -0400

    only allow user to creat events under their own org

--- backend_main/forms.py
+++ backend_main/forms.py
@@ -19,6 +19,10 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ('name', 'description', 'start_date', 'end_date', 'start_time', 'end_time', 'is_public', 'organizer', 'location')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        self.fields['organizer'].queryset = Org.objects.filter(owner_id=user)
         
 class LocationForm(forms.ModelForm):
 
