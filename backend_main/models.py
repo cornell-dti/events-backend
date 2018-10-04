0c5e83b41d3250f047362d1c582a1d1427245836
Arnav Ghosh <ag983@cornell.edu>
2018-10-04 11:25:54 -0400

    Errors with merging, reupload with CSRF USE SESSIONS

---
+++ backend_main/models.py
@@ -0,0 +1,111 @@
+# models.py
+# Arnav Ghosh, Jessica Zhao, Jill Wu, Adit Gupta
+# 17th Sept. 2018
+
+import datetime
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from simple_history.models import HistoricalRecords
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
+
+MAX_NAME_LENGTH = 100
+MAX_DESC_LENGTH = 500
+MAX_TAG_LENGTH = 50
+MAX_CONTACT_LENGTH = 100
+MAX_TOKEN_LENGTH = 2056
+
+class Event(models.Model):
+    name = models.CharField(max_length = MAX_NAME_LENGTH)
+    description = models.CharField(max_length = MAX_DESC_LENGTH)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+<<<<<<< HEAD
+    num_attendees = models.IntegerField(default=0)
+    is_public = models.BooleanField(default=True)
+=======
+    num_attendees = models.IntegerField(default = 0)
+    is_public = models.BooleanField()
+>>>>>>> 48e6835e0dce01adc1795dca4dbef0b8702dd894
+    organizer = models.ForeignKey('Org', on_delete=models.CASCADE)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE)
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length = MAX_TAG_LENGTH)
+
+    def __str__(self):
+        return self.name
+
+class Event_Tags(models.Model):
+    event_id = models.ForeignKey('Event', on_delete=models.CASCADE, related_name = "event_tags")
+    tags_id = models.ForeignKey('Tag',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.event_id, self.tags_id)
+
+
+class Org(models.Model):
+    name = models.CharField(max_length = MAX_NAME_LENGTH)
+    description = models.CharField(max_length = MAX_DESC_LENGTH)
+    contact = models.EmailField(max_length = MAX_CONTACT_LENGTH)
+<<<<<<< HEAD
+    verified = models.BooleanField(default = False)
+=======
+    verified = models.BooleanField()
+>>>>>>> 48e6835e0dce01adc1795dca4dbef0b8702dd894
+    history = HistoricalRecords()
+    owner = models.ForeignKey('auth.User', related_name = 'org', on_delete=models.CASCADE) #user
+
+    def __str__(self):
+        return self.name
+
+class Event_Org(models.Model):
+    event_id = models.ForeignKey('Event', on_delete=models.CASCADE)
+    org_id = models.ForeignKey('Org',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.org_id, self.event_id)
+
+class Location(models.Model):
+    building = models.CharField(max_length = MAX_NAME_LENGTH)
+    room = models.CharField(max_length = MAX_NAME_LENGTH)
+    place_id = models.CharField(max_length = MAX_NAME_LENGTH)
+
+    def __str__(self):
+        return "{0}, {1}".format(self.room, self.building)
+
+class UserID(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length = MAX_TOKEN_LENGTH)
+    #TODO: can a token be stored as a string
+
+class Attendance(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    event_id = models.ForeignKey('Event', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} - {1}".format(self.user_id, self.event_id)
+
+class Media(models.Model):
+    name = models.CharField(max_length = MAX_NAME_LENGTH)
+    file = models.FileField(upload_to="cu_events_images", blank = False)
+    uploaded_by = models.ForeignKey('Org',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Event_Media(models.Model):
+    event_id = models.ForeignKey('Event', on_delete=models.CASCADE, related_name = "event_media")
+    media_id = models.ForeignKey('Media',on_delete=models.CASCADE)
+
+class Org_Media(models.Model):
+    org_id = models.ForeignKey('Org', on_delete=models.CASCADE)
+    media_id = models.ForeignKey('Media',on_delete=models.CASCADE)
---
+++ backend_main/urls.py
@@ -0,0 +1,59 @@
+# urls.py
+# Arnav Ghosh, Jessica Zhao, Jill Wu, Adit Gupta
+# 17th Sept. 2018
+
+from django.conf.urls import url
+from django.conf.urls import include
+<<<<<<< HEAD
+from django.views.decorators.csrf import ensure_csrf_cookie
+=======
+>>>>>>> 48e6835e0dce01adc1795dca4dbef0b8702dd894
+from django.urls import path
+from . import views
+
+dateRegex = "[0-9]{4}[0-1][0-9][0-3][0-9]T[0-9]{6}"
+
+urlpatterns = [
+	path('post/org/', views.OrgFormView.as_view(), name='post_org'),
+	path('post/org/<int:pk>/', views.post_detail_org, name='post_detail_org'),
+	path('post/org/<int:pk>/edit/', views.post_edit_org, name='post_edit_org'),
+
+	path('post/tag/', views.TagFormView.as_view(), name='post_tag'),
+	path('post/tag/<int:pk>/', views.post_detail_tag, name='post_detail_tag'),
+
+	path('post/event/', views.EventFormView.as_view(), name='post_event'),
+	path('post/event/<int:pk>/', views.post_detail_event, name='post_detail_event'),
+
+	path('post/location/', views.LocationFormView.as_view(), name='post_location'),
+	path('post/location/<int:pk>/', views.post_detail_location, name='post_detail_location'),
+
+	url(r'^event/(?P<event_id>[0-9]+)/$', views.EventDetail.as_view(), name='Event Details'),
+	url(r'^org/(?P<org_id>[0-9]+)/$', views.OrgDetail.as_view(), name='Organizer Details'),
+<<<<<<< HEAD
+	url(r'^loc/(?P<location_id>[0-9]+)/$', views.SingleLocationDetail.as_view(), name='Location Details'),
+	url(r'^loc/all/$', views.AllLocationDetail.as_view(), name='All Location Details'),
+=======
+	url(r'^loc/(?P<location_id>[0-9]+)/$', views.LocationDetail.as_view(), name='Location Details'),
+>>>>>>> 48e6835e0dce01adc1795dca4dbef0b8702dd894
+	url(r'^tag/(?P<tag_id>[0-9]+)/$', views.SingleTagDetail.as_view(), name='Single Tag Details'),
+	url(r'^tag/all/$', views.AllTagDetail.as_view(), name='Single Tag Details'),
+	url(r'^media/(?P<img_id>[0-9]+)/$',  views.ImageDetail.as_view(), name='Media Detail'),
+	url(r'^feed/events/timestamp=(?P<in_timestamp>{0})&start=(?P<start_time>{0})&end=(?P<end_time>{0})/$'.format(dateRegex),
+		views.EventFeed.as_view(),
+		name='Updated Event Feed'),
+	url(r'^feed/org/timestamp=(?P<in_timestamp>{0})/$'.format(dateRegex), views.OrgFeed.as_view(), name='Updated Organizer Feed'),
+	url(r'^generate_token/(?P<mobile_id>.*)/$', views.ObtainToken.as_view(), name='Create Mobile Token'),
+	url(r'^attendance/$', views.IncrementAttendance.as_view(), name="Update Attendance"),
+
+	url(r'^users/$', views.UserList.as_view()),
+	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+	url(r'^api-auth/', include('rest_framework.urls')),
+<<<<<<< HEAD
+	url(r'^signup/', ensure_csrf_cookie(views.signup), name="Sign-Up"),
+
+]
+=======
+
+]
+
+>>>>>>> 48e6835e0dce01adc1795dca4dbef0b8702dd894
