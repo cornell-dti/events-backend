40054969bb0f992b5bf51456d771696eeb932d48
Arnav Ghosh <ag983@cornell.edu>
2018-11-08 09:39:16 -0500

    Reset Token

--- backend_main/views.py
+++ backend_main/views.py
@@ -261,11 +261,9 @@ class ObtainToken(APIView):
         if userIDSet.exists():
                 return HttpResponseBadRequest("Token Already Assigned to User")
         else:
-            #validate firebase ID
-            #try:
-            #    idinfo = id_token.verify_oauth2_token(mobile_id, requests.Request(), settings.GOOGLE_BACKEND_CLIENT_ID)
-            #except:
-            #    return HttpResponseBadRequest(idinfo)
+            validated, valid_info = validate_firebase(mobile_id)
+            if not validated:
+                return HttpResponseBadRequest(idinfo)
 
             #generate username
             username = generateUserName()
@@ -280,6 +278,19 @@ class ObtainToken(APIView):
             #generate token
             token = Token.objects.create(user=user)
             return JsonResponse({'token': token.key}, status=status.HTTP_200_OK)
+
+class ResetToken(APIView):
+    #TODO: alter classes to token and admin?
+    permission_classes = (permissions.AllowAny, )
+
+    def get(self, request, mobile_id, format=None):
+        userIDSet = UserID.objects.filter(token=mobile_id)
+        if userIDSet.exists():
+            userID = userIDSet[0]
+            token = Token.objects.get(user = userID.user)
+            return JsonResponse({'token': token.key}, status=status.HTTP_200_OK)
+        else:
+            return HttpResponse("Reset Token Error")
 
 class TagFormView(APIView):
     permission_classes = (permissions.IsAuthenticated, )
@@ -373,6 +384,13 @@ def post_event_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def validate_firebase(mobile_id):
+    try:
+       idinfo = id_token.verify_oauth2_token(mobile_id, requests.Request(), settings.GOOGLE_BACKEND_CLIENT_ID)
+       return True, ""
+    except:
+       return False, idinfo
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.filter(is_staff=False)
