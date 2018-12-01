f341982dc1d93954067bcc6af9548a6d018efb57
Jessica Zhao <j.zhao77@gmail.com>
2018-12-01 14:02:30 -0500

    fix error with post org

--- backend_main/forms.py
+++ backend_main/forms.py
@@ -6,7 +6,7 @@ class OrgForm(forms.ModelForm):
 
     class Meta:
         model = Org
-        fields = ('name', 'description')
+        fields = ('name', 'description', 'verified', 'website', 'photo')
 
 class TagForm(forms.ModelForm):
 
