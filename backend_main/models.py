a72c9908357d7b839348caed8fd5436685d3e55e
Jessica Zhao <j.zhao77@gmail.com>
2019-02-04 17:49:45 -0500

    uncommented some db

--- backend_main/models.py
+++ backend_main/models.py
@@ -115,21 +115,21 @@ class Org_Media(models.Model):
     org_id = models.ForeignKey('Org', on_delete=models.CASCADE)
     media_id = models.ForeignKey('Media',on_delete=models.CASCADE)
 
-# class Profile(models.Model):
-#     org_name = models.CharField(max_length=30, blank=True)
-#     name = models.CharField(max_length=30, blank=True)
-#     netid = models.CharField(max_length=30, blank=True)
-#     facebook = models.CharField(max_length=30, blank=True)
-#     website = models.CharField(max_length=30, blank=True)
-#     contact_us = models.BooleanField(default = False)
-#     verified = models.BooleanField(default = False)
+class Profile(models.Model):
+    org_name = models.CharField(max_length=30, blank=True)
+    name = models.CharField(max_length=30, blank=True)
+    netid = models.CharField(max_length=30, blank=True)
+    facebook = models.CharField(max_length=30, blank=True)
+    website = models.CharField(max_length=30, blank=True)
+    contact_us = models.BooleanField(default = False)
+    verified = models.BooleanField(default = False)
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
