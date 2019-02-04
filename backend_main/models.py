253b535e9ec1324ae6d739dfcdbbecba52f21f84
Jessica Zhao <j.zhao77@gmail.com>
2019-02-04 17:16:39 -0500

    adding one to one profile

--- backend_main/models.py
+++ backend_main/models.py
@@ -125,8 +125,12 @@ class Profile(models.Model):
     contact_us = models.BooleanField(default = False)
     verified = models.BooleanField(default = False)
 
+
 @receiver(post_save, sender=User)
-def update_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
