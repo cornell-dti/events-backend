45130966f0a201eb0f1cecf71d9cccf877035b03
Evan Welsh <2943271+rockon999@users.noreply.github.com>
2019-04-20 20:41:03 -0400

    Prevent passwords from serializing.

--- backend_main/serializers.py
+++ backend_main/serializers.py
@@ -7,16 +7,22 @@ from rest_framework import serializers
 from .models import Event, Org, Location, Tag, Org_Tags, Event_Tags, Event_Media
 from django.contrib.auth.models import User
 
-
+class OrgSerializer(serializers.ModelSerializer):
+	org_tags = serializers.PrimaryKeyRelatedField(queryset = Org_Tags.objects.all(), many=True)
+    
+	class Meta:
+		model = Org
+		fields = ('pk', 'name', 'email', 'bio', 'photo', 'website', 'tags', 'is_staff', 'org_tags', 'is_active')
+		
 class EventSerializer(serializers.ModelSerializer):
     event_tags = serializers.PrimaryKeyRelatedField(queryset = Event_Tags.objects.all(), many=True)
     event_media = serializers.PrimaryKeyRelatedField(queryset = Event_Media.objects.all(), many=True)
-    
+    organizer = OrgSerializer()
+
     class Meta:
         model = Event
-        #exclude = ('history',)
-        fields = ('pk', 'name', 'description', 'start_date', 'end_date', 
-        	'start_time', 'end_time', 'num_attendees', 'is_public', 'organizer', 'location', 'event_tags', 'event_media')
+        fields = ('pk', 'organizer', 'name', 'description', 'start_date', 'end_date', 
+        	'start_time', 'end_time', 'num_attendees', 'is_public', 'location', 'event_tags', 'event_media')
         depth = 1
         
 class LocationSerializer(serializers.ModelSerializer):
@@ -24,13 +30,6 @@ class LocationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Location
 		fields = ('pk', 'building','room', 'place_id')
-
-class OrgSerializer(serializers.ModelSerializer):
-	org_tags = serializers.PrimaryKeyRelatedField(queryset = Org_Tags.objects.all(), many=True)
-
-	class Meta:
-		model = Org
-		fields = ('pk', 'name', 'email', 'bio', 'photo', 'website', 'tags', 'is_staff', 'org_tags', 'is_active')
 
 class TagSerializer(serializers.ModelSerializer):
 
