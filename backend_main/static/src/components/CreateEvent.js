e6c67b65ddb5fa3657cb0e7bb3820c9e0334fdec
Shi Chong <sz329@cornell.edu>
2019-05-10 11:58:47 -0400

    console log response

--- backend_main/static/src/components/CreateEvent.js
+++ backend_main/static/src/components/CreateEvent.js
@@ -138,9 +138,8 @@ class CreateEvent extends Component {
 		xhr.onreadystatechange = function(){
 		    if(xhr.readyState === 4){
 			    if(xhr.status === 200){
-			    	console.log(xhr)
 		        	const response = JSON.parse(xhr.responseText);
-		        	
+		        	console.log(response);
 					xhr = new XMLHttpRequest();
 					xhr.open("POST", response.data.url);
 
