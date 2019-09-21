a92a0ec805e911e3aec7e96e9d6dac3878545fe8
Sam <sam@developersam.com>
2019-09-21 15:33:45 -0400

    Fix fix-heroku branch

--- events_backend/app/apps.py
+++ events_backend/app/apps.py
@@ -1,4 +1,4 @@
 from django.apps import AppConfig
 
 class EventsBackendConfig(AppConfig):
-    name = 'events_backend.app'
+    name = "app"
--- events_backend/app/urls.py
+++ events_backend/app/urls.py
@@ -25,7 +25,7 @@ dateRegex = "[0-9]{4}[0-1][0-9][0-3][0-9]T[0-9]{6}"
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # TODO Should this be events_backend.app.api_urls?
-    url(r'^api/', include('events_backend.app.api_urls')),
+    url(r'^api/', include('app.api_urls')),
 
     #path('post/org/', views.OrgFormView.as_view(), name='post_org'),
     #path('post/org/<int:pk>/', views.post_detail_org, name='post_detail_org'),
