d67ea831d58b1992a7068ff8ec9b7ce07aa806de
Shi Chong <sz329@cornell.edu>
2019-10-05 16:50:56 -0400

    managed to extract all possible data from csv

--- misc/scraper/parse_csv.py
+++ misc/scraper/parse_csv.py
@@ -1,8 +1,11 @@
 import csv
 import os
+import re
+from datetime import datetime, timedelta
+from dateutil.parser import parse
 
 DEFAULT_PASSWORD = 'pbkdf2_sha256$120000$qFamKdatf0if$+aI5iQne/Z+zGJU9EfWcoaaBRUCie/8Rltkz7XRH3fQ='
-EVENTS_FILE_NAME = 'fb_event (4).csv'
+EVENTS_FILE_NAME = 'fb_events (4).csv'
 
 ### COLUMNS, in order ###
 """
@@ -24,36 +27,39 @@ tags (by id and comma separated, so 4,1,9)
 ###########################
 
 if __name__ == '__main__':
-    with open(EVENTS_FILE_NAME, encoding="utf8") as csv_file:
+    with open(EVENTS_FILE_NAME, encoding="utf8", errors="ignore") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
+        first_row_processed = False
+        data_count = 0
+        success_data_count = 0
+        fail_data_count = 0
         for row in csv_reader:
-            web_scraper_order = row[0]
-            web_scraper_start_url = row[1]
+            # web_scraper_order = row[0]
+            # web_scraper_start_url = row[1]
             date = row[2]
-            event = row[3]
-            location_and_time = row[4]
-            url = row[5]
-            url_href = row[6]
+            event_name = row[3]
+            # location_and_time = row[4]
+            # url = row[5]
+            # url_href = row[6] not sure how i can this
             location = row[7]
-            time = row[8]
+            time = row[8]  # retrieve time
             attendance = row[9]
             img_src = row[10]
             description = row[11]
             host = row[12]
-            location_specfic = row[13]
-            tags = ''
+            location_specific = row[13]  # good but not always reliable
+            # tags = ''
             # Tags may be empty cells
-            if len(row) >= 15:
-                tags = row[13]
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
+            # if len(row) >= 15:
+            # tags = row[13]
+            if not first_row_processed:
+                first_row_processed = True
+                continue
             else:
                 """HOW TO MANUALLY ADD AN EVENT
                 0. Manually go through the host column and modify the hosts so that it's only one org
                 For example, it would be "Hosted by _____" instead of "Hosted dby _____ and _______"
-                This is because we want one organization per Host column, and some org names have an "and" in it so
+                This is becau   se we want one organization per Host column, and some org names have an "and" in it so
                 we can't just separate them by "and".
                 1. Manually add tags from the pre-defined set (by id)
                 2. Add an entry for each org into backend_main_user
@@ -61,6 +67,63 @@ if __name__ == '__main__':
                 4. Add an entry into backend_main_media
                 5. Add an entry into backend_main_event_media
                 """
-                print(row)
-                line_count += 1
-        print(f'Processed {line_count} lines.')
+                if host == "" or host == "null":
+                    data_count += 1
+                    fail_data_count += 1
+                    continue
+
+                if event_name == "" or event_name == "null":
+                    data_count += 1
+                    fail_data_count += 1
+                    continue
+
+                if (location == "" or location == "null") and (location_specific == "" or location_specific == "null"):
+                    data_count += 1
+                    fail_data_count += 1
+                    continue
+
+                start_datetime, end_datetime = re.findall(
+                    "(?! ).*?\d+[\:\d\d]*\s[A|P]M", time)
+
+                start_time, end_time = re.findall(
+                    "\d+[\:\d{2}]*\s[A|P]M", time)
+
+                try:
+                    if ((parse(start_datetime) - parse(date + " " + start_time)).total_seconds() != 0):
+                        start_datetime = date + " " + start_time
+                except:
+                    start_datetime = date + " " + start_time
+
+                if end_datetime == end_time:
+                    end_datetime = date + " " + end_time
+                else:
+                    end_datetime = end_datetime
+
+                try:
+                    if (parse(end_datetime) - parse(start_datetime)).total_seconds() < 0:
+                        end_datetime = end_date
+                except:
+                    continue
+
+                event_name = event_name
+                location = location
+                location_specific = location_specific
+                start_date = datetime.strftime(
+                    parse(start_datetime), "%Y-%m-%d")
+                start_time = datetime.strftime(
+                    parse(start_datetime), "%H:%M:%S")
+                end_date = datetime.strftime(
+                    parse(end_datetime), "%Y-%m-%d")
+                end_time = datetime.strftime(
+                    parse(end_datetime), "%H:%M:%S")
+                description = description
+
+                host = re.search(
+                    "(?![Hosted by]).*", host).group()
+                attendance = attendance
+                img_src = img_src
+
+                data_count += 1
+                success_data_count += 1
+        print(
+            f'Processed {data_count} lines of data. {success_data_count} success. {fail_data_count} failure.')
