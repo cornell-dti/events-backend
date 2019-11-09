cee391bb2b456414bd8390f5b8a541ea6b506cc7
Enochen <sirenochen@gmail.com>
2019-11-09 16:51:47 -0500

    Stop fetching script from writing invalid events to json

--- misc/scraper/localist_fetch.py
+++ misc/scraper/localist_fetch.py
@@ -2,10 +2,61 @@ import json
 
 import requests
 
+
+def is_valid_event(event_id):
+    try:
+        event = event_id['event']
+
+        event_name = event['title']
+        description = event['description_text']
+        location = event['location_name']
+        room = event['room_number']
+        latitude = event['geo']['latitude']
+        longitude = event['geo']['longitude']
+
+        img_src = event['photo_url']
+        all_day = event['event_instances'][0]['event_instance']['all_day']
+
+        start = event['event_instances'][0]['event_instance']['start']
+        end = event['event_instances'][0]['event_instance']['end']
+
+        # How do we handle?
+        if all_day and end is None:
+            end = start
+
+        if event_name == "" or event_name is None:
+            print("Missing name")
+            return False
+
+        if description == "" or description is None:
+            print("Missing desc")
+            return False
+
+        if (location == "" or location is None) and (room == "" or room is None):
+            print("Missing location or room")
+            return False
+
+        if start == "" or start is None or end == "" or end is None:
+            print("Missing datetime")
+            return False
+
+        if (latitude == "" or latitude is None or longitude == "" or longitude is None) and (
+                location == "" or location is None):
+            print("Missing location AND coordinates")
+            return False
+
+        if img_src == "" or img_src is None:
+            print("Missing image link")
+            return False
+    except:
+        return False
+    return True
+
+
 if __name__ == '__main__':
     MAX_EVENT_PER_PAGE = 100
-    SCRAPER_START_DATE = "2019-10-30"
-    SCRAPER_END_DATE = "2019-10-31"
+    SCRAPER_START_DATE = "2019-10-01"
+    SCRAPER_END_DATE = "2019-11-04"
 
     URL = f"https://events.cornell.edu/api/2/events?start={SCRAPER_START_DATE}&end={SCRAPER_END_DATE}&pp={MAX_EVENT_PER_PAGE}"
 
@@ -29,90 +80,5 @@ if __name__ == '__main__':
                 event['event']['tags'] = []
             data['events'].extend(extra['events'])
             page += 1
-        for event_id in data["events"]:
-            try:
-                event = event_id['event']
-
-                event_name = event['title']
-                description = event['description_text']
-                location = event['location_name']
-                room = event['room_number']
-                latitude = event['geo']['latitude']
-                longitude = event['geo']['longitude']
-
-                img_src = event['photo_url']
-                all_day = event['event_instances'][0]['event_instance']['all_day']
-
-                start = event['event_instances'][0]['event_instance']['start']
-                end = event['event_instances'][0]['event_instance']['end']
-
-                # How do we handle?
-                if all_day and end is None:
-                    end = start
-
-                org_name = event['filters']['departments'][0]['name']
-
-                contact_email = "donotdisplay@cornell.edu"
-                try:
-                    contact_email = event['custom_fields']['contact_email']
-                except KeyError:
-                    print("using dummy email")
-
-                if event_name == "" or event_name is None:
-                    print("Missing name")
-                    data_count += 1
-                    fail_data_count += 1
-                    continue
-
-                if description == "" or description is None:
-                    print("Missing desc")
-                    data_count += 1
-                    fail_data_count += 1
-                    continue
-
-                if (location == "" or location is None) and (room == "" or room is None):
-                    print("Missing location or room")
-                    data_count += 1
-                    fail_data_count += 1
-                    continue
-
-                if start == "" or start is None or end == "" or end is None:
-                    print("Missing datetime")
-                    data_count += 1
-                    fail_data_count += 1
-                    continue
-
-                if (latitude == "" or latitude is None or longitude == "" or longitude is None) and (
-                        location == "" or location is None):
-                    print("Missing location AND coordinates")
-                    data_count += 1
-                    fail_data_count += 1
-                    continue
-
-                if img_src == "" or img_src is None:
-                    print("Missing image link")
-                    data_count += 1
-                    fail_data_count += 1
-                    continue
-
-                if org_name == "" or org_name is None:
-                    print("Missing organizer name")
-                    data_count += 1
-                    fail_data_count += 1
-                    continue
-
-                if (contact_email == "" or contact_email is None):
-                    print("Missing organizer email")
-                    contact_email = "donotdisplay@cornell.edu"
-                    continue
-
-                if room is None:
-                    room = ''
-
-                data_count += 1
-                success_data_count += 1
-            except:
-                data["events"].remove(event_id)
-                data_count += 1
-                fail_data_count += 1
+        data["events"] = [x for x in data["events"] if is_valid_event(x)]
         json.dump(data, f)
